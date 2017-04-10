fb3942093fa24edffc5625678423933bd36e6d11
ricardoschullerSL <rschuller@scottlogic.com>
2017-04-10 15:35:33 +0100

    updated packages.json to include body-parser

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-// import {Route, IndexRoute, MemoryRouter } from "react-router";
 import Layout from "./components/Layout";
 import { Provider } from "react-redux";
 
@@ -12,25 +11,3 @@ const app = document.getElementById("app");
 
 ReactDOM.render(<Provider store={store}>
     <Layout /></Provider>, app);
-
-
-/*(function() {
-    var app = angular.module("ChatApp", []);
-
-    app.controller("ChatController", function($scope, $http) {
-        $scope.loggedIn = false;
-
-        $http.get("/api/user").then(function(userResult) {
-            $scope.loggedIn = true;
-            $scope.user = userResult.data;
-            $http.get("/api/users").then(function(result) {
-                $scope.users = result.data;
-            });
-        }, function() {
-            $http.get("/api/oauth/uri").then(function(result) {
-                $scope.loginUri = result.data.uri;
-            });
-        });
-    });
-})();
-*/
